a3fa08a8ed2f6b8e60154325547b1f806e2a47b7
Martin <martin.crisostomo@sansano.usm.cl>
2020-10-01 00:29:39 -0400

    hero

--- resources/pasillo.py
+++ resources/pasillo.py
@@ -84,7 +84,7 @@ class Pasillo(Resource):
                         dic['camaras'].append({'id_camara':i.id,'url_foto':i.path})
                         products = []
                         for j in productos:
-                            products.append({'nombre_producto':j.product_name , 'coordenadas': j.coordinates})
+                            products.append({'id':j.id,'nombre_producto':j.product_name , 'coordenadas': j.coordinates})
                         
                         dic['camaras'][-1].update({'productos': products})
                     return dic ,200
